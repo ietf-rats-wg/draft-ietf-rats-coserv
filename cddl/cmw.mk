6d8928aba987ef654e144f09ad017aaf5284281a
Thomas Fossati <thomas.fossati@linaro.org>
2025-12-11 15:29:23 +0100

    chore: update CDDL deps
    * CMW-23
    * CoRIM-09
    
    Signed-off-by: Thomas Fossati <thomas.fossati@linaro.org>

--- cddl/cmw.mk
+++ cddl/cmw.mk
@@ -1,7 +1,7 @@
 # CMW CDDL
 github := https://github.com/ietf-rats-wg/
 cmw_rel_dl := draft-ietf-rats-msg-wrap/releases/download/
-cmw_tag := cddl-draft-ietf-rats-msg-wrap-14
+cmw_tag := cddl-draft-ietf-rats-msg-wrap-23
 cmw_url := $(join $(github), $(join $(cmw_rel_dl), $(cmw_tag)))
 
 cmw-autogen.cddl: ; $(curl) -LO $(cmw_url)/$@
